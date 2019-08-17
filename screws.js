c62534beb766f8e05565e3085b42ed0a37c7fb75
Jalia Evans <irohdesign@outlook.com>
2019-08-17 13:03:48 -0400

    successfully scraping from individual page tables and specs

--- screws.js
+++ screws.js
@@ -1,78 +1,134 @@
 const puppeteer = require('puppeteer');
-const $ = require('jquery');
-
-var StrippedString;
-
-function stripTags(toStrip) {
-    StrippedString = toStrip.replace(/(<([^>]+)>)/ig," ");
-    return StrippedString;
-}
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.mcmaster.com/93235a074',  {waitUntil: 'networkidle2'});
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.mcmaster.com/93734a120', {waitUntil: 'networkidle2'});
 
-  let bodyHandle;
-//   for title
-  bodyHandle = await page.$('h3.header-primary--pd');
-  let title = await page.evaluate(body => body.innerHTML, bodyHandle);
-  title = stripTags(title);
+  // const bodyHandle = await page.$('body');
+  // const html = await page.evaluate(body => body.textContent, bodyHandle);
+  // await bodyHandle.dispose();
 
-  let listing = {
-    'name': title,
-  };
+  // console.log(html);
+  let table = await page.evaluate(() => 
+  Array.from(document
+    .querySelectorAll('.spec-table--pd tbody'))
+    .map(partner => partner.innerHTML)
+    );
 
-  bodyHandle = await page.$('tbody');
-  let table = await page.evaluate(body => body.innerHTML, bodyHandle);
-  
-table = table.split('</tr>');
-let rowData = [];
+  table = table[0];
 
-table.forEach(function (row) {
-    rowPieces = row.split('</td>');
-    rowPieces.forEach(function (datapoint) {
-        datapoint = stripTags(datapoint).trim();
-        if (datapoint) {
-            rowPieces = [];
-            rowData.push(datapoint);
-        }
-    })
-    console.log(rowPieces);
-});
+  newTable = table.split('</tr>');
+  // console.log(newTable);
 
+  // if row has multiple children
+  let children = [];
+  let valueArr = [];
+  let obj = {};
+  let title;
+  let titleArr = [];
+  let value;
+  let valueStorage = [];
+  let weirdValue;
+  let objArr = [];
 
+  newTable.forEach(function (row) {
+    if ((row.includes('child-attr--table')) || (row.includes('grouped-row--table'))) {
+      let newRow = row.split('</td>');
+      // valueArr = [];
 
-// console.log(listing);
-// let newRowData = [];
-// rowData.forEach(function(row) {
-//     row.forEach(function (individual) {
-//         individual = stripTags(individual).trim();
-//         newRowData.push(individual);
-//     });
-// })
-// console.log(newRowData);
-// console.log(table);
-//   table = stripTags(table).split('  ');
-//   let qual = table[0].trim();
-//   let detail = table[1].trim();
+      newRow.forEach(function (item) {
+        if (item.includes('<tr')) {
+          let datapoint = item.split('>');
+          datapoint = datapoint[2].trim();
+          children.push(datapoint);
+        } else if (item.includes('<td')) {
+          let value = item.split('>');
+          value = value[1];
+          valueArr.push(value);
+        }
+      })
+      children.forEach(function (instance, index) {
+        obj[instance] = valueArr[index];
+      })
+    } else {
+      let rows = row.split('<tr');
+      rows.forEach(function (row) {
+        let data = row.split('<td>');
+        data.forEach(function (instance) {
+          if (instance.includes('row--spec-tbl')) {
+            let content = instance.split('</td>');
+            content.forEach(function (line) {
+              if (line.includes('row--spec-tbl')) {
+                title = line.split('>');
+                title = title[2].trim();
+                titleArr.push(title);
 
-//     listing[qual] = detail;
-//     console.log(listing);
+              } else if (line.includes('value-cell--table')) {
+                value = line.split('<td');
 
-// for table
-// await page.goto('https://www.mcmaster.com/screws',  {waitUntil: 'networkidle2'});
-// bodyHandle = await page.$('.GroupPrsnttn h3');
+                value.forEach(function (search) {
+                  if (search.includes('span')) {
+                    let span = search.split('>');
+                    span = span[2];
+                    value = span.split('<');
+                    value = value[0];
+                    weirdValue = value;
+                  }
+                });
 
-// let tableTitle = await page.evaluate(body => body.innerHTML, bodyHandle);
+                value = value[1];
+                value = value.split('>');
+                value = value[1];
+                valueStorage.push(value);
+              }
+            })
+          }
+        })
+      })
+    }
+  })
+  // console.log(titleArr.length);
+  // console.log(valueStorage.length);
 
+  titleArr.forEach(function (title, index) {
+    obj = {};
+    obj[title] = valueStorage[index];
+    objArr.push(obj);
+  })
+  console.log(objArr);
 
-// bodyHandle = await page.$('.GroupPrsnttn');
+  // table.forEach(function (row) {
+  //   let data = row.split('</td>');
+  //   let itemArr = [];
+  //   let title, value;
+  //   let obj = {};
 
-//     let row = await page.evaluate(body => body.innerHTML, bodyHandle);
-//   row = stripTags(row);
+  //   data.forEach(function(item) {
+  //     if (item.includes('attr-cell--table')) {
+  //       title = item.split('>').pop();
+  //     } else if (item.includes('value-cell--table')) {
+  //       value = item.split('>').pop();
+  //     }
+  //   })
+  //   obj[title] = value;
+  //   objArr.push(obj);
+  // })
+  // console.log(objArr);
 
-//   console.log(row);
+  const categories = await page.evaluate(() => 
+    Array.from(document
+      .querySelectorAll('.attr-cell--table'))
+      .map(partner => partner.innerText.trim())
+      );
 
-  await browser.close();
+    const values = await page.evaluate(() => 
+    Array.from(document
+      .querySelectorAll('.value-cell--table'))
+      .map(partner => partner.innerText.trim())
+      );
+
+    // console.log(categories);
+    // console.log(table);
+  await browser.close();
 })();
